0836a872a6087cea85d2a52020f6f806f66d72f0
Kellie MacPhee <kmacphee@uw.edu>
2018-04-28 19:50:41 -0700

    Moving data and image files back into docs

---
+++ docs/data_gathering/variables.mk
@@ -0,0 +1,18 @@
+# High-level variables for the Makefile
+
+#-------------------------------------------------------------------------
+# 	Only change these if you want to re-run everything for a new data set
+#-------------------------------------------------------------------------
+
+
+# Sex of team we're interested in (W/M)
+SEX = W
+
+# The year of the games we're interested in
+YEAR = 2017
+
+# Number of games in the set of games we are interested in
+NUM_GAMES = 48
+
+# Type of files we are analyzing (play-by-play --> "hr" (no others supported yet))
+PDF_TYPE = hr
